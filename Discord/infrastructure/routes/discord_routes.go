dc426f0e5f2f504d225124a4546813566dff4d7a
carlos22101 <233429@ids.upchiapas.edu.mx>
2025-03-24 10:44:08 -0600

    implementación de rutas

--- Discord/infrastructure/routes/discord_routes.go
+++ Discord/infrastructure/routes/discord_routes.go
@@ -1,1 +1,12 @@
 package routes
+
+
+
+import (
+    "github.com/gin-gonic/gin"
+    "RecuNgrok/discord/infrastructure/controllers"
+)
+
+func SetupWebhookRoutes(router *gin.Engine, handler *controllers.WebhookHandler) {
+    router.POST("/webhook", handler.ProcessEvent)
+}
