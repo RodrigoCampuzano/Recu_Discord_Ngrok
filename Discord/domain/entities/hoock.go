8049334a7812dbc1e639628987dda4a441899198
Rodrigo Campuzano <camcluod@gmail.com>
2025-03-24 01:07:21 -0600

    Definicion de Entidades

--- Discord/domain/entities/hoock.go
+++ Discord/domain/entities/hoock.go
@@ -1,1 +1,15 @@
 package entities
+
+type EventData struct {
+    Action      string `json:"action"`
+    PullRequest *struct {
+        Title string `json:"title,omitempty"`
+        State string `json:"state,omitempty"`
+        URL   string `json:"html_url,omitempty"`
+    } `json:"pull_request,omitempty"`
+    Workflow *struct {
+        Status     string `json:"status,omitempty"`
+        Conclusion string `json:"conclusion,omitempty"`
+        URL        string `json:"html_url,omitempty"`
+    } `json:"workflow_run,omitempty"`
+}
