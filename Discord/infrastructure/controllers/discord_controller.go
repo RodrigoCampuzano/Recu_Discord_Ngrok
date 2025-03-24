946ed2f658cbdf2969ef447cb11968738894358c
Rodrigo Campuzano <camcluod@gmail.com>
2025-03-24 02:35:58 -0600

    Controladores y manejo de webhooks

--- Discord/infrastructure/controllers/discord_controller.go
+++ Discord/infrastructure/controllers/discord_controller.go
@@ -1,1 +1,36 @@
 package controllers
+
+import (
+    "log"
+    "net/http"
+    "github.com/gin-gonic/gin"
+    "webhook_multi/cmd/app/usecases"
+    "webhook_multi/cmd/domain/entities"
+)
+
+type WebhookHandler struct {
+    eventProcessor *usecases.EventProcessor
+}
+
+func NewWebhookHandler(processor *usecases.EventProcessor) *WebhookHandler {
+    return &WebhookHandler{eventProcessor: processor}
+}
+
+func (wh *WebhookHandler) ProcessEvent(ctx *gin.Context) {
+    var event entities.EventData
+
+    if err := ctx.ShouldBindJSON(&event); err != nil {
+        log.Println("Error parsing JSON:", err)
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+        return
+    }
+
+    log.Println("Webhook received:", event)
+
+    channel, message := wh.eventProcessor.HandleEvent(event)
+    if channel != "" {
+        ctx.JSON(http.StatusOK, gin.H{"channel": channel, "message": message})
+    } else {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to process event"})
+    }
+}
